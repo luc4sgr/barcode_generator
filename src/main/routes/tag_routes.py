eee655a291cef3ffa1889cd385a84bbe887b276c
luc4sgr <23.lucasdoliveira@gmail.com>
2025-04-03 08:17:18 -0300

    Enhance create_tags route with validation and error handling

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,18 +1,23 @@
 from flask import Blueprint, request, jsonify
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
-tags_routes_bp = Blueprint('tag_routes', __name__)
 
-@tags_routes_bp.route('/create_tag', methods=['POST'])
+from src.errors.error_handler import handle_errors
+
+from src.validators.tag_creator_validator import tag_creator_validator
+
+tags_routes_bp = Blueprint('tags_routes', __name__)
+
+@tags_routes_bp.route('/create_tag', methods=["POST"])
 def create_tags():
-    tag_creator_view = TagCreatorView()
-    print("Creating tags...")
-    # Logic to create tagsp
-    print(request.json)
-    http_request = HttpRequest(
-        # header=request.headers,
-        body=request.json,
-        # query_params=request.args
-    )
-    respose = tag_creator_view.validate_and_create(http_request)
-    return jsonify(respose.body), respose.status_code
+    response = None
+    try:
+        tag_creator_validator(request)
+        tag_creator_view = TagCreatorView()
+
+        http_request = HttpRequest(body=request.json)
+        response = tag_creator_view.validate_and_create(http_request)
+    except Exception as exception:
+        response = handle_errors(exception)
+
+    return jsonify(response.body), response.status_code
